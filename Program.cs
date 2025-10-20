947bd19c35a8ed42acd367d90c176956b7bbabf1
RobyDoge <147256897+RobyDoge@users.noreply.github.com>
2025-10-20 12:22:38 +0300

    Implemented the base of the menu

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using MiniDeepThought.Util;
+
+do
+{
+    int option = ConsoleHelpers.OptionSelector();
+    switch (option)
+    {
+        case 1:
+            ConsoleHelpers.SubmitQuestion();
+            break;
+        case 2:
+            ConsoleHelpers.ListJobs();
+            break;
+        case 3:
+            ConsoleHelpers.ViewResultByJobId();
+            break;
+        case 4:
+            ConsoleHelpers.CancelJob();
+            break;
+        case 5:
+            return;
+        default:
+            break;
+
+    }
+
+
+} while (true);
---
+++ Util/ConsoleHelpers.cs
@@ -0,0 +1,41 @@
+﻿namespace MiniDeepThought.Util;
+
+internal static class ConsoleHelpers
+{
+    public static int OptionSelector()
+    {
+        Console.WriteLine();
+        Console.WriteLine("""
+        Please choice an input:
+        1. Submit Question
+        2. List Jobs
+        3. View Results by JobId
+        4. Cancel Running Job
+        5. Exit
+        """);
+        if (!int.TryParse(Console.ReadLine(), out int result))
+        {
+            Console.WriteLine("The input must be a number");
+            return 0;
+        }
+        if (result < 1 || result > 5)  Console.WriteLine("Invalid input range.");
+        return result;
+    }
+    public static void SubmitQuestion()
+    {
+        throw new NotImplementedException();
+    }
+    public static void ListJobs()
+    {
+        throw new NotImplementedException();
+    }
+    public static void ViewResultByJobId()
+    {
+        throw new NotImplementedException();
+    }
+    public static void CancelJob()
+    {
+        throw new NotImplementedException();
+    }
+
+}
